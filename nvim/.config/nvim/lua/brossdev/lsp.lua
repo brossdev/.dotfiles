b212459e7fa2463ab0cee23478e3157d9126b5ec
bobby <bobby.dev@pm.me>
2023-03-09 07:30:11 +0000

    fix: syntax for lua keymaps

--- nvim/.config/nvim/lua/brossdev/lsp.lua
+++ nvim/.config/nvim/lua/brossdev/lsp.lua
@@ -34,10 +34,10 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-  buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap(bufnr, 'n', '<space>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 --
 -- debugging
   buf_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", opts)
