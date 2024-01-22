49bc681554bbb0ee190443d743e6658326f604c4
bobby <bobby.dev@pm.me>
2024-01-22 15:43:59 +0000

    feat: add lualine config

---
+++ nvim/.config/nvim/lua/brossdev/lazy/lualine.lua
@@ -0,0 +1,30 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            options = {
+                theme = 'auto',
+                icons_enabled = true
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            extensions = { 'fugitive' }
+        }
+    end
+}
